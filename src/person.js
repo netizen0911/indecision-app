e2bf9383afd1a4689292ba81451132d68ab64947
Paul G <paulo.guetan@gmail.com>
2018-11-12 22:10:12 +0800

    Added import and export exercise files.

---
+++ src/person.js
@@ -0,0 +1,20 @@
+// function isAdult(age) {
+//   if (age >= 18) {
+//     return true
+//   } else {
+//     return false
+//   }
+// }
+
+// function canDrink(age) {
+//   if (age >= 21) {
+//     return true
+//   } else {
+//     return false
+//   }
+// }
+
+const isAdult = (age) => age >= 18;
+const canDrink = (age) => age >= 21;
+
+export { isAdult, canDrink}
